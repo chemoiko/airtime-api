7afb9af112cc58fdd49f4e4bfd0082d17596bbb5
Murket Elijah Chemoiko <129403454+chemoiko@users.noreply.github.com>
2024-12-29 14:45:23 +0300

    Update urls.py

--- Edoctor_AirtimeAPI/edoctor_airtimeapi/apidashboard/urls.py
+++ Edoctor_AirtimeAPI/edoctor_airtimeapi/apidashboard/urls.py
@@ -3,29 +3,31 @@ from . import views
 from django.conf import settings
 
 
-urlpatterns = [
-    path('auth/register', views.register),  # Registration API endpoint
-    path('auth/login', views.loginUser),    # Login API endpoint
-    path('auth/logout', views.logoutUser),
-    path('login', views.loginView),
-    path('', views.redirectUser, name="redirect"),  # Root path
-    path('dashboard', views.dashboard),  # Updated path for the dashboard
-    path('add_admin', views.addAdmin),
-    re_path(r'^.*html', views.dashboard),  # Regex for matching HTML files
-    path('apikey/generate_apikey', views.generateAPICredentials),
-    path('logs', views.getLogs),
-    path('requestDocVerification', views.requestVerifyDocs),
-    path('verifyCompany', views.verifyDoc),
-    path('getDoc', views.getVerificationDoc),
-    path('getDocs', views.getVerificationDocs),
-    path('verifyKey', views.verifyKey),
-    path('disableKey', views.disableKey),
-    path('public/TC', views.getPublic),
-    path('public/services', views.getPublic),
-    path('public/faq', views.getPublic),
-    path('public/aboutus', views.getPublic),
-    path('public/contactus', views.getPublic),
-    path('public/privacy', views.getPublic),
-    # path('', views.redirect)
+urlpatterns = [  
+    path('/auth/register',views.register), #registration api endpoint
+    path('/auth/login',views.loginUser), #registration api endpoint
+    path('/auth/logout',views.logoutUser),
+    path('/login', views.loginView),
+    #re_path(r'^$'
+    path('',views.redirectUser, name="redirect"),
+    path('/',views.dashboard),
+    path('/add_admin',views.addAdmin),
+    re_path(r'^/.*html',views.dashboard),
+    path("/apikey/generate_apikey",views.generateAPICredentials),
+    path("/logs",views.getLogs),
+    #path("/verify_requests", views.verifyDocs),
+    path("/requestDocVerification", views.requestVerifyDocs),
+    path("/verifyCompany",views.verifyDoc),
+    path("/getDoc",views.getVerificationDoc),
+    path("/getDocs",views.getVerificationDocs),
+    path("/verifyKey",views.verifyKey),
+    path("/disableKey",views.disableKey),
+    path('/public/TC', views.getPublic),
+    path('/public/services', views.getPublic),
+    path('/public/faq', views.getPublic),
+    path('/public/aboutus', views.getPublic),
+    path('/public/contactus', views.getPublic),
+    path('/public/privacy', views.getPublic),
+    #path('', views.redirect)
 
 ]
